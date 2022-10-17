f1fc84e280495c457542a1f5eeadb2f1aaa1e319
PezzottiCarlo <carlo.pezzotti@student.supsi.ch>
2022-10-17 21:37:43 +0200

    refactor: :recycle: refact code

--- server/server.js
+++ server/server.js
@@ -64,75 +64,65 @@ app.get('/download/:repo', async (req, res) => {
 app.get('/build/:repo', async (req, res) => {
     let repoName = req.params.repo;
     console.log("Building repo: " + repoName);
-    if (github.isRepoDownloaded(repoName)) {
-        if (await github.isBuildable(repoName)) {
-            if (await github.buildRepo(repoName))
-                return res.send({ success: true });
-            else
-                return res.send({ success: false, message: "Error while building repo" });
-        }
+    if (!github.isRepoDownloaded(repoName))
+        return res.send({ success: false, message: "Repo not downloaded" });
+    if (!await github.isBuildable(repoName))
         return res.send({ success: false, message: "Repo not buildable" });
-    }
-    else return res.send({ success: false, message: "Repo not downloaded" });
+    if (await github.buildRepo(repoName))
+        return res.send({ success: true });
+    else
+        return res.send({ success: false, message: "Error while building repo" });
 })
 
 app.get('/keepUpdate/:repo/:flag', async (req, res) => {
     let hook;
     let kUFlag = req.params.flag === 'true';
-    if (!keepUpdateTmp[req.params.repo]) {
-        hook = await github.setWebhook(req.params.repo, config.payloadUrl, kUFlag, config.token);
-        if (hook.errors) {
-            console.log(hook.errors);
-            res.json({ success: false, message: "Error while setting webhook" });
-            return
-        } else {
-            keepUpdateTmp[req.params.repo] = {};
-            let tmp = {
-                state: kUFlag,
-                hookId: hook.id
-            }
-            keepUpdateTmp[req.params.repo] = tmp;
-        }
-    } else {
+    if (keepUpdateTmp[req.params.repo]) {
         keepUpdateTmp[req.params.repo].state = kUFlag;
+        Utility.setKeepUpdate(keepUpdateTmp);
+        return res.json({ success: true });
     }
+    hook = await github.setWebhook(req.params.repo, config.payloadUrl, kUFlag, config.token);
+    if (hook.errors) {
+        return res.json({ success: false, message: "Error while setting webhook" });
+    }
+    keepUpdateTmp[req.params.repo] = {};
+    let tmp = {
+        state: kUFlag,
+        hookId: hook.id
+    }
+    keepUpdateTmp[req.params.repo] = tmp;
     Utility.setKeepUpdate(keepUpdateTmp);
     res.json({ success: true });
 })
 
 
-//TO DO: check whether the signature on the request is correct
 app.post('/github', async (req, res) => {
     const hmac = req.headers['x-hub-signature-256'];
     const verified = github.verifySignature(JSON.stringify(req.body), hmac);
-    if (verified) {
-        let pushedInfo = req.body
-        if (pushedInfo && pushedInfo.repository) {
-            if (keepUpdateTmp[pushedInfo.repository.name] && keepUpdateTmp[pushedInfo.repository.name].state) {
-                console.log("Updating...", pushedInfo.repository.name);
-                await github.pullRepo(pushedInfo.repository.name);
-                console.log("Search a build configuration...");
-                if (await github.isBuildable(pushedInfo.repository.name)) {
-                    console.log("Building...");
-                    if (await github.buildRepo(pushedInfo.repository.name))
-                        console.log("Build successfull");
-                    else {
-                        console.log("Build failed");
-                        res.sendStatus(500);
-                        return;
-                    }
-                } else {
-                    console.log('Not buildable');
-                }
-                console.log("Updated");
+    if (!verified)
+        return res.status(401).send('Unauthorized');
+    let pushedInfo = req.body
+    if (!pushedInfo && !pushedInfo.repository)
+        return res.status(400).send('Bad request');
+    if (keepUpdateTmp[pushedInfo.repository.name] && keepUpdateTmp[pushedInfo.repository.name].state) {
+        console.log("Updating...", pushedInfo.repository.name);
+        await github.pullRepo(pushedInfo.repository.name);
+        console.log("Search a build configuration...");
+        if (await github.isBuildable(pushedInfo.repository.name)) {
+            console.log("Building...");
+            if (await github.buildRepo(pushedInfo.repository.name))
+                console.log("Build successfull");
+            else {
+                console.log("Build failed");
+                res.sendStatus(500);
+                return;
             }
         } else {
-            console.log("No repository info");
+            console.log('Not buildable');
         }
-        res.sendStatus(200);
-    } else {
-        console.log("Signature not verified");
-        res.sendStatus(403);
+        console.log("Updated");
     }
+    res.sendStatus(200);
 })
 
